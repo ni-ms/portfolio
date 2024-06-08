f1a53f2762542e9f0195432a41da1c543f9ae0d0
nikmsharma <nikmsharma@gmail.com>
2024-06-08 13:18:20 +0530

    set portfolio as base

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,8 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: '/portfolio'
 		}
 	}
 };

9cb7320e409d296b0060e463fbf88b514e531e2e
nikmsharma <nikmsharma@gmail.com>
2024-06-08 23:59:57 +0530

    Checking if hyperlinks work

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 // const path = process.env.BASE_PATH || '/portfolio';
-const path = '/portfolio';
+// const path = '/portfolio';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors

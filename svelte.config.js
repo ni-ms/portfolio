353106bcf1b2ff8972018b84dff216b19a246667
nikmsharma <nikmsharma@gmail.com>
2024-06-08 14:20:37 +0530

    Glitch with portfolio ; sometimes the hyperlinks dont work...

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const path = process.env.BASE_PATH || '/portfolio';
+// const path = process.env.BASE_PATH || '/portfolio';
+const path = '/portfolio';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
